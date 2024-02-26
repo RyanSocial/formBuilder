e015364bb9e94ed90c053c7a93b9f8e0f9190780
RyanSocial <Ryan@socialscientist.co.za>
2024-02-26 15:57:42 +0200

    New : Global Control

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {MarketReportComponent} from "./products/emails/market-report/market-report.component";
+import {InputComponent} from "./UI/form-controls/input/input.component";
+import {GlobalControlComponent} from "./UI/global-control/global-control.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MarketReportComponent],
+  imports: [RouterOutlet, MarketReportComponent, InputComponent, GlobalControlComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
