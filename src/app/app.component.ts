aa68c7f2c61afb6007d3ba92f2367a293be44a81
RyanSocial <Ryan@socialscientist.co.za>
2024-02-26 10:40:51 +0200

    New : market Report Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {MarketReportComponent} from "./products/emails/market-report/market-report.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MarketReportComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
