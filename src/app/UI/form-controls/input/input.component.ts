4537f1b88ad8b9234f5865bf3eb3351050c0d13b
RyanSocial <Ryan@socialscientist.co.za>
2024-02-27 14:00:29 +0200

    Update: styling for form

--- src/app/UI/form-controls/input/input.component.ts
+++ src/app/UI/form-controls/input/input.component.ts
@@ -3,6 +3,7 @@ import {MatFormField, MatFormFieldModule} from "@angular/material/form-field";
 import {FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
 import {MatInputModule} from "@angular/material/input";
 import {QuestionBase} from "../../../questions/questions.base";
+import {NgClass} from "@angular/common";
 
 @Component({
   selector: 'app-input',
@@ -10,7 +11,8 @@ import {QuestionBase} from "../../../questions/questions.base";
   imports: [
     MatFormFieldModule,
     MatInputModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    NgClass
   ],
   templateUrl: './input.component.html',
   styleUrl: './input.component.css'
@@ -19,9 +21,19 @@ export class InputComponent {
   @Input({required: true}) question!: QuestionBase<string>
   @Input({required: true}) form!: FormGroup
 
-  isFieldInvalid(question:string):boolean {
+  isFieldInvalid(question: string): string {
     const control = this.form.get(question);
-    return control! && control.touched && control.invalid;
+   if(control! && control.touched && control.invalid) {
+     return 'border-r-4 border-r-red-500'
+   }
+   return ''
+  }
+
+  warningStyle(key: string): string {
+    if (this.isFieldInvalid(key)) {
+      return `text-gray-300 text-sm`
+    }
+    return ''
   }
 
 }
