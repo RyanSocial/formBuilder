7ed0fa5c3d25457d6bd7986c87f0cda7e21da357
RyanSocial <Ryan@socialscientist.co.za>
2024-02-27 11:20:27 +0200

    Update : remove unused

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {MarketReportComponent} from "./products/emails/market-report/market-report.component";
 import {InputComponent} from "./UI/form-controls/input/input.component";
-import {GlobalControlComponent} from "./UI/global-control/global-control.component";
+
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MarketReportComponent, InputComponent, GlobalControlComponent],
+  imports: [RouterOutlet, MarketReportComponent, InputComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
