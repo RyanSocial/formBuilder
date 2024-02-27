38395110c3016272cb2e2c31a45888fc54ed6a99
RyanSocial <Ryan@socialscientist.co.za>
2024-02-27 13:41:08 +0200

    New : dynamic placeholder text

--- src/app/questions/questions.base.ts
+++ src/app/questions/questions.base.ts
@@ -1,4 +1,11 @@
+
+
 export class QuestionBase<T> {
+
+  private createPlaceholder(key: string): string {
+    return key.split("_").map(word => word[0].toUpperCase() + word.slice(1)).join(" ");
+  }
+
   value: T | undefined;
   key: string;
   label: string;
@@ -29,19 +36,22 @@ export class QuestionBase<T> {
     nestedFormGroup?:QuestionBase<any>[]
     hide?: boolean
 
+
   } = {}) {
     this.value = options.value;
     this.key = options.key || '';
-    this.label = options.label || '';
+    this.label = this.createPlaceholder(this.key)
     this.required = !!options.required;
     this.order = options.order === undefined ? 1 : options.order;
     this.controlType = options.controlType || '';
     this.type = options.type || '';
     this.options = options.options || [];
     this.checked = options.checked || false
-    this.placeholder = options.placeholder
     this.nestedQuestions = options.nestedQuestions || [];
     this.nestedFormGroup = options.nestedFormGroup || []
     this.hide = options.hide || false
+    this.placeholder = options.placeholder || this.createPlaceholder(this.key)
   }
 }
+
+
