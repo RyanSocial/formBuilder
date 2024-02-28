d9ab549b2120e7eff95b57577b042648af61d929
RyanSocial <Ryan@socialscientist.co.za>
2024-02-28 18:39:18 +0200

    Update : Brokers list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,35 +3,27 @@ import {RouterOutlet} from '@angular/router';
 import {MarketReportComponent} from "./products/emails/market-report/market-report.component";
 import {InputComponent} from "./UI/form-controls/input/input.component";
 import {DropdownComponent} from "./UI/form-controls/dropdown/dropdown.component";
-import {BrokersService} from "./shared/api/brokers.service";
+import {BrokersService} from "./shared/api/brokers/brokers.service";
 import {Broker} from "../models/broker.interface";
-
+import {BrokersListComponent} from "./brokers/brokers-list/brokers-list.component";
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
   providers: [BrokersService],
-  imports: [RouterOutlet, MarketReportComponent, InputComponent, DropdownComponent],
+  imports: [RouterOutlet, MarketReportComponent, InputComponent, DropdownComponent, BrokersListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  brokers = signal<Broker[] | undefined>([])
-  showDefer = signal<boolean>(false)
+  brokerList: Broker[] = []
 
   constructor(private brokersService: BrokersService) {
     this.brokersService.getBrokers().subscribe({
-      next: brokers => this.brokers.set(brokers)
+      next: brokers => {
+        console.log('Broker recieved', brokers)
+        this.brokerList = brokers}
     })
-setTimeout(() => {
-  this.showDefer.set(true)
-},3000)
   }
-
-  setDefer() {
-    this.showDefer.set(true)
-  }
-
-
 }
