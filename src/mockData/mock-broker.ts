358a0fa97749d80655caf3cc276bcee36b55642e
RyanSocial <Ryan@socialscientist.co.za>
2024-02-28 10:14:54 +0200

    New : Mock data

---
+++ src/mockData/mock-broker.ts
@@ -0,0 +1,42 @@
+import {Broker} from "../models/broker.interface";
+
+export const mockBrokers: Broker[] = [
+  {
+    "_id": "5db03981873f00615648459c",
+    "broker_id": 681,
+    "db_name": "pg_acaweb_v",
+    "active": false,
+    "deactivation_date": 1678956963648,
+    "note": "Cancelled by divesh@autochartist.com",
+    "name": "10x",
+    "solr_use_df_offset": false,
+    "has_mobile": false,
+    "solr_xmlapi_collection": null,
+    "performancestatstype": null,
+    "realm_config": null,
+    "logo": null,
+    "mobile": {
+      "show_login": false,
+      "has_mobile": false,
+      "mobile_chart_style": null,
+      "mobile_lead_generation_web_hook": null,
+      "has_va": false
+    }
+  },
+  {
+    "_id": "54aaa106c26f20e9048c95c8",
+    "broker_id": 365,
+    "db_name": "pg_acaweb_v",
+    "active": false,
+    "deactivation_date": 1678957061195,
+    "note": "Cancelled by divesh@autochartist.com",
+    "name": "24bulls",
+    "solr_use_df_offset": false,
+    "has_mobile": false,
+    "solr_xmlapi_collection": null,
+    "performancestatstype": null,
+    "realm_config": null,
+    "logo": null,
+    "mobile": null
+  },
+]
