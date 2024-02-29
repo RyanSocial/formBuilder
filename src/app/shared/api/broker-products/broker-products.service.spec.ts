99b1d31d10e96a87f297243b0d7b1a87c67f9c1d
RyanSocial <Ryan@socialscientist.co.za>
2024-02-29 11:55:00 +0200

    Update : Test for product API

--- src/app/shared/api/broker-products/broker-products.service.spec.ts
+++ src/app/shared/api/broker-products/broker-products.service.spec.ts
@@ -1,16 +1,40 @@
-import { TestBed } from '@angular/core/testing';
-
-import { BrokerProductsService } from './broker-products.service';
+import {TestBed} from '@angular/core/testing';
+import {BrokerProductsService} from './broker-products.service';
+import {HttpClientTestingModule, HttpTestingController} from "@angular/common/http/testing";
+import {BrokerProductInterface} from "../../../../models/broker-product.interface";
+import {mockBrokerProducts} from "../../../../mockData/mock-broker-product";
 
 describe('BrokerProductsService', () => {
-  let service: BrokerProductsService;
-
+  let brokerProductsService: BrokerProductsService;
+  let httpTestingController: HttpTestingController
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(BrokerProductsService);
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [BrokerProductsService]
+    });
+    brokerProductsService = TestBed.inject(BrokerProductsService);
+    httpTestingController = TestBed.inject(HttpTestingController)
+  });
+
+  afterEach(() => {
+    httpTestingController.verify()
   });
 
   it('should be created', () => {
-    expect(service).toBeTruthy();
+    expect(brokerProductsService).toBeTruthy();
   });
+  describe('Get broker Products', () => {
+    it('should return an array of all broker products', () => {
+      let brokerProducts: BrokerProductInterface | undefined
+      // use ID 24 to match mocked data
+      brokerProductsService.getBrokerProducts(24).subscribe({
+        next: response => {
+          brokerProducts = response
+        }
+      })
+      const req = httpTestingController.expectOne('https://datafactory.autochartist.com/api/v1/mongo_config/_table/broker_product/?where_clause_filter=broker_id%3D24&limit=50&include_count=false&count_only=false')
+      req.flush(mockBrokerProducts)
+      expect(brokerProducts).toEqual(mockBrokerProducts)
+    })
+  })
 });
---
+++ src/mockData/mock-broker-product.ts
@@ -0,0 +1,90 @@
+import {BrokerProductInterface} from "../models/broker-product.interface";
+
+export const mockBrokerProducts: BrokerProductInterface = {
+  "resource": [
+    {
+      "broker_id": "24",
+      "product_name": "TRADING_OPPORTUNITIES_WEB_COMPONENT"
+    },
+    {
+      "broker_id": "24",
+      "product_name": "OUR_FAVOURITES_WEB_COMPONENT"
+    },
+    {
+      "broker_id": "24",
+      "product_name": "VOLATILITY_ANALYSIS_WEB_COMPONENT"
+    },
+    {
+      "broker_id": "24",
+      "product_name": "RISK_CALCULATOR_WEB_COMPONENT"
+    },
+    {
+      "broker_id": "24",
+      "product_name": "NEWS_EVENT_TRADING_SERVICE_WEB_COMPONENT"
+    },
+    {
+      "broker_id": "24",
+      "product_name": "EVENTS_CALENDAR_WEB_COMPONENT"
+    },
+    {
+      "broker_id": "24",
+      "product_name": "NEWS_WEB_COMPONENT"
+    },
+    {
+      "broker_id": "24",
+      "product_name": "RESEARCH_PORTAL_WEB_COMPONENT"
+    },
+    {
+      "broker_id": "24",
+      "product_name": "MARKET_REPORT_WEB_COMPONENT"
+    },
+    {
+      "broker_id": "24",
+      "product_name": "MARKET_REPORT_EMAIL"
+    },
+    {
+      "broker_id": "24",
+      "product_name": "HIGH_IMPACT_NEWS_EVENTS_EMAIL"
+    },
+    {
+      "broker_id": "24",
+      "product_name": "TRADE_OF_THE_DAY_EMAIL"
+    },
+    {
+      "broker_id": "24",
+      "product_name": "TECHNICAL_ANALYSIS_EMAIL"
+    },
+    {
+      "broker_id": "24",
+      "product_name": "TOP_5_TRADES_OF_LAST_WEEK_EMAIL"
+    },
+    {
+      "broker_id": "24",
+      "product_name": "CORRELATING_ALERTS_EMAIL"
+    },
+    {
+      "broker_id": "24",
+      "product_name": "SALES_CONVERSATION_EMAIL"
+    },
+    {
+      "broker_id": "24",
+      "product_name": "INTEGRATION_INTO_CUSTOMER_EMAIL"
+    },
+    {
+      "broker_id": "24",
+      "product_name": "MT_4_5_OPPORTUNITY_SCANNER_PLATFORM"
+    },
+    {
+      "broker_id": "24",
+      "product_name": "MT_4_5_RISK_CALCULATOR_PLATFORM"
+    },
+    {
+      "broker_id": "24",
+      "product_name": "MOBILE_APP_PLATFORM"
+    },
+    {
+      "broker_id": "24",
+      "product_name": "C_TRADER_PLATFORM"
+    }
+  ]
+}
