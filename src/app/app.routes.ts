4ab3d63b3bc9776ebf038bbc91499dfcfd32719a
RyanSocial <Ryan@socialscientist.co.za>
2024-03-01 09:57:50 +0200

    Update : App Routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
+import {BrokersListComponent} from "./brokers/brokers-list/brokers-list.component";
+import {PageNotFoundComponent} from "./UI/page-not-found/page-not-found.component";
+import {EmailsComponent} from "./products/emails/emails.component";
+import {MarketReportComponent} from "./products/emails/market-report/market-report.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: 'ac-broker-manager', component: BrokersListComponent},
+  {path: '', redirectTo: '/ac-broker-manager', pathMatch: 'full'},
+  {
+    path: 'emails',
+    component: EmailsComponent,
+    children: [{
+      path: 'market-reports', // child route path
+      component: MarketReportComponent, // child route component that the router renders
+    },]
+  },
+  {path: '**', component: PageNotFoundComponent},  // Wildcard route for a 404 page
+];
