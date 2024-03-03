a18baec6877537d121d2c2f8c4daea51692d88ec
RyanSocial <Ryan@socialscientist.co.za>
2024-03-03 17:04:12 +0200

    Update : selected Broker

--- src/app/UI/nav/nav-bar/nav-bar.component.ts
+++ src/app/UI/nav/nav-bar/nav-bar.component.ts
@@ -1,6 +1,8 @@
-import {Component} from '@angular/core';
+import {Component, signal} from '@angular/core';
 import {MenuLinkInterface} from "../../../../models/menu-link.interface";
 import {RouterLink} from "@angular/router";
+import {Broker} from "../../../../models/broker.interface";
+import {SelectBrokerService} from "../../../shared/services/select-broker/select-broker.service";
 
 
 @Component({
@@ -13,6 +15,11 @@ import {RouterLink} from "@angular/router";
   styleUrl: './nav-bar.component.css'
 })
 export class NavBarComponent {
+  constructor(private selectBrokerService: SelectBrokerService) {
+  }
+
+  selectedBroker = this.selectBrokerService.currentBroker
+
   routes: MenuLinkInterface[] = [{
     name: 'Brokers', linkRef: 'ac-broker-manager',
   },
