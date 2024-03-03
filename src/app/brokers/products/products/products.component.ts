09041c582a779ab961188149c4ea3e426a197109
RyanSocial <Ryan@socialscientist.co.za>
2024-03-03 18:37:35 +0200

    Update: Web Components

--- src/app/brokers/products/products/products.component.ts
+++ src/app/brokers/products/products/products.component.ts
@@ -1,16 +1,19 @@
 import {Component, computed, EventEmitter, input, OnInit, Output, signal} from '@angular/core';
 import {Broker} from "../../../../models/broker.interface";
 import {BrokerProductsService} from "../../../shared/api/broker-products/broker-products.service";
-import {JsonPipe} from "@angular/common";
+import {JsonPipe, NgIf} from "@angular/common";
 import {SelectBrokerService} from "../../../shared/services/select-broker/select-broker.service";
 import {Product} from "../../../../models/broker-product.interface";
 import {Router} from "@angular/router";
+import {FormatProductTitlePipe} from "../../../pipes/product/format-product-title.pipe";
 
 @Component({
   selector: 'app-products',
   standalone: true,
   imports: [
-    JsonPipe
+    JsonPipe,
+    NgIf,
+    FormatProductTitlePipe
   ],
   templateUrl: './products.component.html',
   styleUrl: './products.component.css'
@@ -28,10 +31,12 @@ export class ProductsComponent implements OnInit {
     this.broker.set(this.selectBrokerService.getSelectedBroker())
   }
 
+
+
   ngOnInit() {
     this.brokerProductsService.getBrokerProducts(this.broker()?.broker_id!).subscribe({
+
       next: value => {
-        console.log(value)
         this.products.set(value)
       }
     })
@@ -40,8 +45,17 @@ export class ProductsComponent implements OnInit {
   emailProducts = computed(() => {
     return this.products()?.filter(product => product.product_name.toLowerCase().includes('email'))
   })
+  // Includes mobile app
+  WebProducts = computed(() => {
+    return this.products()?.filter(product => !product.product_name.toLowerCase().includes('email'))
+  })
+
+
 
   configureEmail() {
     this.router.navigate(['/emails'], {state: {emails: this.emailProducts()}});
+  }
+  configureProducts() {
+    this.router.navigate(['/products'], {state: {emails: this.WebProducts()}});
   }
 }
