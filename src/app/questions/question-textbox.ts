e536ff7304ebd58aef681ff064233ccb1c471ae4
RyanSocial <Ryan@socialscientist.co.za>
2024-03-11 20:24:53 +0200

    Update : Input type

--- src/app/questions/question-textbox.ts
+++ src/app/questions/question-textbox.ts
@@ -1,5 +1,5 @@
 import {QuestionBase} from "./questions.base";
 
-export class TextboxQuestion extends QuestionBase<string> {
+export class TextboxQuestion extends QuestionBase<string | number> {
     override controlType = 'textbox';
 }
