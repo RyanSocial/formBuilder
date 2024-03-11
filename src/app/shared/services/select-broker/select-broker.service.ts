cdc49a9c678054ed52c9b84e033147730c4a9c01
RyanSocial <Ryan@socialscientist.co.za>
2024-03-11 20:25:54 +0200

    Update: Email Selection

--- src/app/shared/services/select-broker/select-broker.service.ts
+++ src/app/shared/services/select-broker/select-broker.service.ts
@@ -1,6 +1,8 @@
 import {Injectable, signal} from '@angular/core';
 import {Broker} from "../../../../models/broker.interface";
 import {mockBrokers} from "../../../../mockData/mock-broker";
+import {BehaviorSubject, Observable} from "rxjs";
+import {Product} from "../../../../models/broker-product.interface";
 
 @Injectable({
   providedIn: 'root'
@@ -8,13 +10,21 @@ import {mockBrokers} from "../../../../mockData/mock-broker";
 export class SelectBrokerService {
   private selectedBroker = signal<Broker>(mockBrokers[0])
 
+  private emailProducts = new BehaviorSubject<Product[] | undefined>(undefined)
+  emailProducts$ = this.emailProducts as Observable<Product[]>
+
   readonly currentBroker = this.selectedBroker.asReadonly()
+
   setBroker(broker: Broker) {
     this.selectedBroker.set(broker)
   }
 
-  getSelectedBroker(): Broker  {
+  getSelectedBroker(): Broker {
     return this.currentBroker()
+  }
+
+  updateEmails(emails: Product[] | undefined): void {
+    this.emailProducts.next(emails)
   }
 
 
