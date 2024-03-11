7e90eeec0331110e21ca6554ec09ebe49af04383
RyanSocial <Ryan@socialscientist.co.za>
2024-03-11 20:26:19 +0200

    Update: Array Form Class

--- src/app/questions/question-formArray.ts
+++ src/app/questions/question-formArray.ts
@@ -3,4 +3,5 @@ import {QuestionBase} from "./questions.base";
 
 export class FormGroupArray extends QuestionBase<string> {
     override type = 'FormGroupArray';
+    override simpleArray: boolean = true
 }
