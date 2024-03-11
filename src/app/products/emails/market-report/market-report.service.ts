bb7cdedb2ff40989f2625a7fa5f4fac342d3df24
RyanSocial <Ryan@socialscientist.co.za>
2024-03-11 20:26:58 +0200

    Update: Market Report Form

--- src/app/products/emails/market-report/market-report.service.ts
+++ src/app/products/emails/market-report/market-report.service.ts
@@ -5,6 +5,7 @@ import {FormGroupQuestion} from "../../../questions/question-formGroup";
 import {of} from "rxjs";
 import {ToggleQuestion} from "../../../questions/question-toggle";
 import {DropdownQuestion} from "../../../questions/question-dropdown";
+import {FormGroupArray} from "../../../questions/question-formArray";
 
 @Injectable()
 export class MarketReportService {
@@ -18,39 +19,98 @@ export class MarketReportService {
         key: 'name',
         required: true,
       }),
-      new TextboxQuestion({
-        key: 'original_execution',
-        required: true,
-        hide: true
-
+      // new TextboxQuestion({
+      //   key: 'report_id',
+      // }),
+      // new TextboxQuestion({
+      //   key: 'paired_report_id',
+      //   required: true,
+      // }),
+      // new TextboxQuestion({
+      //   key: 'preserve_published_date',
+      //   required: true,
+      // }),
+      // new TextboxQuestion({
+      //   key: 'original_execution',
+      //   required: true,
+      // }),
+      // new ToggleQuestion({
+      //   key: 'error_on_empty',
+      //   required: true,
+      //   value: false
+      // }),
+      // new ToggleQuestion({
+      //   key: 'use_legacy',
+      //   required: true,
+      //   value: false
+      // }),
+      // new ToggleQuestion({
+      //   key: 'generate_market_movements',
+      //   required: true,
+      //   value: false
+      // }),
+      // new ToggleQuestion({
+      //   key: 'show_mobile_link',
+      //   required: true,
+      //   value: false
+      // }),
+      // new TextboxQuestion({
+      //   key: 'trades_to_persist',
+      //   required: true,
+      // }),
+      // new TextboxQuestion({
+      //   key: 'trades_to_persist_filter_number_of_hours_back',
+      //   required: true,
+      // }),
+      // new ToggleQuestion({
+      //   key: 'trades_to_persist_filter_pattern_type',
+      //   required: true,
+      //   value: false
+      // }),
+      new FormGroupQuestion({
+        key: 'messages',
+        nestedFormGroup: [
+          new ToggleQuestion({
+            key: 'randomized_analysis',
+            required: true,
+            value: false
+          }),
+          new ToggleQuestion({
+            key: 'include_forecast',
+            required: true,
+            value: false
+          })
+        ]
       }),
-      new ToggleQuestion({
-        key: 'preserve_published_date',
-        required: true,
-        checked:true
+      new FormGroupQuestion({
+        key: 'filter',
+        nestedFormGroup: [
+          new ToggleQuestion({
+            key: 'active',
+            required: true,
+            value: false
+          }),
+          new TextboxQuestion({
+            key: 'opportunities_per_data_set',
+            required: true,
+            value: 0
+          }), new TextboxQuestion({
+            key: 'filter_symbols_hours',
+            required: true,
+            value: 24
+          }),
+          new TextboxQuestion({
+            key: 'filter_opportunities_hours',
+            required: true,
+            value: 168
+          }),
+        ]
       }),
-      new ToggleQuestion({
-        key: 'Error on Empty',
+      new FormGroupArray({
+        key: 'testFormArray',
         required: true,
-        value:true
-      }),
-      new ToggleQuestion({
-        key: 'Use Legacy',
-        required: true,
-        value: true
-      }),
-      new ToggleQuestion({
-        key: 'Generate Market Movements',
-        required: true,
-
-      }),
-      new ToggleQuestion({
-        key: 'Show Mobile Link',
-        required: true,
-      }),
-      new DropdownQuestion({
-        key: 'Locales',
-        options: [{ key: '1', value: 'uk' }]
+        simpleArray:true,
+        options: [{key : 'Option 1', value: '1'}, {key: 'Option 2', value: '2'}]
       })
 
     ]
