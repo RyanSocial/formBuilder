0ddc9058aa7fc7796f5aab542100828d47cce159
RyanSocial <Ryan@socialscientist.co.za>
2024-03-15 12:30:14 +0200

    Refactor : simplify

--- src/app/products/emails/market-report/market-report.component.ts
+++ src/app/products/emails/market-report/market-report.component.ts
@@ -1,4 +1,4 @@
-import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
+import {ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit} from '@angular/core';
 import {FormGroup, FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {MarketReportService} from "./market-report.service";
 import {QuestionBase} from "../../../questions/questions.base";
@@ -7,7 +7,7 @@ import {QuestionControlService} from "../../../shared/services/question-control-
 import {AsyncPipe, JsonPipe} from "@angular/common";
 import {checkType} from "../../../../utils";
 import {InputComponent} from "../../../UI/form-controls/input/input.component";
-import {FormControllerComponent} from "../../../UI/form-controls/form-controller/form-controller.component";
+import {MrTestComponent} from "../mr-test/mr-test.component";
 
 
 @Component({
@@ -19,38 +19,22 @@ import {FormControllerComponent} from "../../../UI/form-controls/form-controller
     ReactiveFormsModule,
     JsonPipe,
     InputComponent,
-    FormControllerComponent,
+    MrTestComponent,
+
   ],
   providers: [MarketReportService, QuestionControlService],
   templateUrl: './market-report.component.html',
   styleUrl: './market-report.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class MarketReportComponent implements OnInit {
-  questions$ = new BehaviorSubject<QuestionBase<any>[]>([]);
-  markReportForm!: FormGroup;
+export class MarketReportComponent {
+
+  markReportForm: FormGroup = new FormGroup({})
   payLoad: string = ''
-
-  constructor(
-    private marketReportService: MarketReportService,
-    private questionControlService: QuestionControlService
-  ) {
-  }
-
-  ngOnInit() {
-    // Retrieve questions data in ngOnInit
-    const questions = this.marketReportService.getMarketReportQuestions();
-    this.questions$.next(questions);
-
-    // Create the form group after the questions are available
-    this.markReportForm = this.questionControlService.toFormGroup(this.questions$.getValue() as QuestionBase<string>[]);
-
-  }
 
   onSubmit() {
     this.payLoad = JSON.stringify(this.markReportForm.getRawValue());
   }
 
 
-  protected readonly checkType = checkType;
 }
