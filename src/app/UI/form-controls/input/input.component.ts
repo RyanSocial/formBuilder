e79e4eafb312ab5d09f7bad604b41068c83a5f3f
RyanSocial <Ryan@socialscientist.co.za>
2024-03-15 12:29:59 +0200

    Refactor : simplify

--- src/app/UI/form-controls/input/input.component.ts
+++ src/app/UI/form-controls/input/input.component.ts
@@ -1,5 +1,5 @@
 import {Component, inject, Input, input} from '@angular/core';
-import {MatFormField, MatFormFieldModule} from "@angular/material/form-field";
+import {MatFormFieldModule} from "@angular/material/form-field";
 import {ControlContainer, FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
 import {MatInputModule} from "@angular/material/input";
 import {QuestionBase} from "../../../questions/questions.base";
@@ -19,27 +19,10 @@ import {NgClass} from "@angular/common";
   viewProviders: [
     {
       provide: ControlContainer,
-      useFactory: () => inject(ControlContainer,{skipSelf:true})
+      useFactory: () => inject(ControlContainer, {skipSelf: true})
     }
   ]
 })
 export class InputComponent {
-  @Input({required: true}) question!: QuestionBase<string>
-
-
-  // isFieldInvalid(question: string): string {
-  //   const control = this.form.get(question);
-  //  if(control! && control.touched && control.invalid) {
-  //    return 'border-r-4 border-r-red-500'
-  //  }
-  //  return ''
-  // }
-
-  // warningStyle(key: string): string {
-  //   if (this.isFieldInvalid(key)) {
-  //     return `text-gray-300 text-sm`
-  //   }
-  //   return ''
-  // }
-
+  @Input({required: true}) question!: QuestionBase<string | number | boolean>
 }
