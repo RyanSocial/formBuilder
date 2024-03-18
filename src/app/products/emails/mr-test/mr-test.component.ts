ec596691e3bda1672ff9efdde775504b9da623b8
RyanSocial <Ryan@socialscientist.co.za>
2024-03-18 08:49:47 +0200

    Update : new param to app-input

--- src/app/products/emails/mr-test/mr-test.component.ts
+++ src/app/products/emails/mr-test/mr-test.component.ts
@@ -1,16 +1,27 @@
-import {Component, inject, OnInit} from '@angular/core';
+import {AbstractType, Component, ElementRef, inject, OnInit, viewChild, ViewChild} from '@angular/core';
 import {MarketReportService} from "../market-report/market-report.service";
 import {QuestionBase} from "../../../questions/questions.base";
 import {ControlContainer, FormArray, FormBuilder, FormControl, FormGroup, ReactiveFormsModule} from "@angular/forms";
 import {InputComponent} from "../../../UI/form-controls/input/input.component";
+import {JsonPipe, NgForOf, ViewportScroller} from "@angular/common";
 
+interface Execution {
+  type: string;
+  tasks: Task[];
+}
+
+interface Task extends FormControl {
+  name: string; // Or other properties as needed
+}
 
 @Component({
   selector: 'app-mr-test',
   standalone: true,
   imports: [
     InputComponent,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    NgForOf,
+    JsonPipe
   ],
   templateUrl: './mr-test.component.html',
   styleUrl: './mr-test.component.css',
@@ -31,8 +42,12 @@ export class MrTestComponent implements OnInit {
   messageControls: QuestionBase<string | number | boolean>[] = []
   nestedHeadingMessageControls: QuestionBase<string | number | boolean>[] = []
   nestedReportNameMessageControls: QuestionBase<string | number | boolean>[] = []
+  testForm: FormGroup
 
-  constructor(private marketReportService: MarketReportService, private fb: FormBuilder) {
+  constructor(
+    private marketReportService: MarketReportService,
+    private fb: FormBuilder,
+    private viewportScroller: ViewportScroller) {
     this.formControls = marketReportService.getFormControlQuestions()
     this.filterControls = marketReportService.getFilterQuestions()
     this.chartControls = marketReportService.getChartQuestions()
@@ -40,19 +55,46 @@ export class MrTestComponent implements OnInit {
     this.messageControls = marketReportService.getMessageQuestions()
     this.nestedHeadingMessageControls = marketReportService.getMessageHeadingQuestions()
     this.nestedReportNameMessageControls = marketReportService.getMessageReportNAme()
-
+    this.testForm = new FormGroup({
+      skills: new FormArray([
+        new FormControl(),
+        new FormControl(),
+        new FormControl()
+      ])
+    })
   }
+
+  ngOnInit() {
+    this.setAllControls()
+  }
+
 
   get parentFormGroup() {
     return this.parentContainer.control as FormGroup
   }
 
+  controls: { [key: string]: FormControl } = {
+// Will be populated with a list of getters in the setFormControls method
+  }
+
+
+  getControl(key: string): FormControl {
+    return this.controls[key]
+  }
+
+  getNestedControl(groupName: string, controlName: string): FormControl {
+    return this.parentFormGroup.get(groupName)?.get(controlName) as FormControl
+  }
+
+
   // USED FOR LISTS OF FORM CONTROL QUESTIONS
   setFormControls() {
     this.formControls.forEach(control => {
-      this.parentFormGroup.addControl(control.key, new FormControl())
+      this.parentFormGroup.addControl(control.key, new FormControl(control.value ? control.value : ''))
+      this.controls[control.key] = this.parentFormGroup.get(control.key) as FormControl
     })
   }
+
 
   // ADD FORM GROUPS WITH SIMPLE CONTROLS
   setFormGroup(groupName: string, questionArray: QuestionBase<string | number | boolean>[]): void {
@@ -72,7 +114,7 @@ export class MrTestComponent implements OnInit {
   setExecutions() {
     const group = this.fb.group({
       type: [],
-      tasks: [[]]
+      tasks: new FormArray<Task>([])
     })
     this.parentFormGroup.addControl("executions", new FormArray([group]))
   }
@@ -81,9 +123,6 @@ export class MrTestComponent implements OnInit {
     this.parentFormGroup.addControl("data_sets", new FormArray([]))
   }
 
-  ngOnInit() {
-    this.setAllControls()
-  }
 
   setAllControls() {
 
@@ -108,4 +147,10 @@ export class MrTestComponent implements OnInit {
     this.setExecutions()
     this.setDataSet()
   }
+
+  logControl(parent: any) {
+    // console.log(parent.controls.get('tasks'))
+  }
+
+  protected readonly FormGroup = FormGroup;
 }
