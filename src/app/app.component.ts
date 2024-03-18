638dd66cd831fce2210541edc011c7e905eaed54
RyanSocial <Ryan@socialscientist.co.za>
2024-03-18 15:50:54 +0200

    New : global error

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,13 +6,14 @@ import {InputComponent} from "./UI/form-controls/input/input.component";
 
 import {BrokersListComponent} from "./brokers/brokers-list/brokers-list.component";
 import {NavBarComponent} from "./UI/nav/nav-bar/nav-bar.component";
+import {GlobalErrorHandlingComponent} from "./UI/error-handling/global-error-handling/global-error-handling.component";
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
 
-  imports: [RouterOutlet, MarketReportComponent, InputComponent, BrokersListComponent, NavBarComponent],
+  imports: [RouterOutlet, MarketReportComponent, InputComponent, BrokersListComponent, NavBarComponent, GlobalErrorHandlingComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,16 @@
-import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import {ApplicationConfig} from '@angular/core';
+import {provideRouter} from '@angular/router';
 
-import { routes } from './app.routes';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import {HttpClientModule, provideHttpClient, withFetch} from "@angular/common/http";
+import {routes} from './app.routes';
+import {provideAnimationsAsync} from '@angular/platform-browser/animations/async';
+import {HTTP_INTERCEPTORS, HttpClientModule, provideHttpClient, withFetch} from "@angular/common/http";
+import {AppHttpInterceptor} from "./UI/error-handling/app-http/app-http.interceptor";
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimationsAsync(),provideHttpClient((withFetch()))]
+  providers: [
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    provideHttpClient((withFetch())),
+    {provide: HTTP_INTERCEPTORS, useClass: AppHttpInterceptor, multi: true}
+  ]
 };
