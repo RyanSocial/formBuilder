702e51ccf204a5a9eed61c45dd5011e8fe889966
RyanSocial <Ryan@socialscientist.co.za>
2024-04-10 08:22:59 +0200

    Update : Change Market-Report to Styling

--- src/app/styling/emails/market-report/market-report-styling/market-report-styling.component.ts
+++ src/app/styling/emails/market-report/market-report-styling/market-report-styling.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 import {InfoComponent} from "../../components/body/info/info.component";
+import {AcLogoComponent} from "../../components/body/ac-logo/ac-logo.component";
+import {VisualInfoComponent} from "../../components/body/visual-info/visual-info.component";
 
 @Component({
   selector: 'app-market-report-styling',
   standalone: true,
   imports: [
-    InfoComponent
+    InfoComponent,
+    AcLogoComponent,
+    VisualInfoComponent
   ],
   templateUrl: './market-report-styling.component.html',
   styleUrl: './market-report-styling.component.css'
