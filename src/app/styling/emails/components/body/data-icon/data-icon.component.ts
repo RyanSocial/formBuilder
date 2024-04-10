3c3b3f5828d9713505b3d0cd4f8672f8752354b8
RyanSocial <Ryan@socialscientist.co.za>
2024-04-10 08:08:49 +0200

    New : Data Icon for email

---
+++ src/app/styling/emails/components/body/data-icon/data-icon.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-data-icon',
+  standalone: true,
+  imports: [],
+  templateUrl: './data-icon.component.html',
+  styleUrl: './data-icon.component.css'
+})
+export class DataIconComponent {
+
+}
