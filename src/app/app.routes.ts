00b06ead5ae5de459a8ba62107d4bede6eb4aaab
RyanSocial <Ryan@socialscientist.co.za>
2024-07-01 11:33:10 +0200

    UPDATE: QUESTION SERVICE

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,14 +3,12 @@ import {BrokersListComponent} from "./brokers/brokers-list/brokers-list.componen
 import {PageNotFoundComponent} from "./UI/page-not-found/page-not-found.component";
 import {EmailsComponent} from "./products/emails/emails.component";
 import {ProductsComponent} from "./brokers/products/products/products.component";
-import {RssTestComponent} from "./rss-test/rss-test.component";
 import {RssComponent} from "./products/emails/rss/rss.component";
 import {MarketReportsComponent} from "./products/emails/market-reports/market-reports.component";
 
 export const routes: Routes = [
   {path: 'ac-broker-manager', component: BrokersListComponent},
   {path: '', redirectTo: '/ac-broker-manager', pathMatch: 'full'},
-  {path: 'test-rss', component: RssTestComponent},
   {
     path: 'emails/:broker_id',
     component: EmailsComponent,
