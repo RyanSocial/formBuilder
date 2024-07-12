641cc2538701c5593f4ade8e7e113632727ac85d
RyanSocial <Ryan@socialscientist.co.za>
2024-07-12 13:28:46 +0200

    new: Spinner component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,13 +6,14 @@ import {InputComponent} from "./UI/form-controls/input/input.component";
 import {BrokersListComponent} from "./brokers/brokers-list/brokers-list.component";
 import {NavBarComponent} from "./UI/nav/nav-bar/nav-bar.component";
 import {GlobalErrorHandlingComponent} from "./UI/error-handling/global-error-handling/global-error-handling.component";
+import {SpinnerComponent} from "./UI/spinner/spinner.component";
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
 
-  imports: [RouterOutlet, InputComponent, BrokersListComponent, NavBarComponent, GlobalErrorHandlingComponent],
+    imports: [RouterOutlet, InputComponent, BrokersListComponent, NavBarComponent, GlobalErrorHandlingComponent, SpinnerComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
