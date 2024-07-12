64fdcad1d1f4d8aa787166e32ba067ad7c5d076a
RyanSocial <Ryan@socialscientist.co.za>
2024-07-12 14:45:15 +0200

    new: setup environments

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  baseApiUrl: "https://bzcbpgg4oxuotmmjwjmyqymxuu0ommvr.lambda-url.eu-west-1.on.aws/",
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  baseApiUrl: "https://rwrqtcrh4wskgkmmu564f2jlfq0sivnh.lambda-url.eu-west-1.on.aws",
+}
