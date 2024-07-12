b74e29631046fdafee7278ddd6109ced9820ad29
RyanSocial <Ryan@socialscientist.co.za>
2024-07-12 13:28:29 +0200

    update: test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -18,6 +18,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, form_builder');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, SwiftMailFrontend');
   });
 });
