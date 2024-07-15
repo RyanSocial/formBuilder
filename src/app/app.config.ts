33f817ad19b00a6c15f4793443a377fda4987205
RyanSocial <Ryan@socialscientist.co.za>
2024-07-15 06:25:20 +0200

    update: interceptor

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,8 @@
 import {ApplicationConfig} from '@angular/core';
 import {provideRouter} from '@angular/router';
-
 import {routes} from './app.routes';
 import {provideAnimationsAsync} from '@angular/platform-browser/animations/async';
-import {HTTP_INTERCEPTORS, HttpClientModule, provideHttpClient, withFetch} from "@angular/common/http";
+import {HTTP_INTERCEPTORS, provideHttpClient, withFetch} from "@angular/common/http";
 import {AppHttpInterceptor} from "./UI/error-handling/app-http/app-http.interceptor";
 
 export const appConfig: ApplicationConfig = {
