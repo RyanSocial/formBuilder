72c0021c163279951de5789cc6a1afbde4ef88bd
RyanSocial <Ryan@socialscientist.co.za>
2024-07-15 06:24:56 +0200

    update: tidy code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,15 @@
 import {Component} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
-import {InputComponent} from "./UI/form-controls/input/input.component";
-
-
 import {BrokersListComponent} from "./brokers/brokers-list/brokers-list.component";
 import {NavBarComponent} from "./UI/nav/nav-bar/nav-bar.component";
 import {GlobalErrorHandlingComponent} from "./UI/error-handling/global-error-handling/global-error-handling.component";
-import {SpinnerComponent} from "./UI/spinner/spinner.component";
+
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-
-    imports: [RouterOutlet, InputComponent, BrokersListComponent, NavBarComponent, GlobalErrorHandlingComponent, SpinnerComponent],
+  imports: [RouterOutlet, BrokersListComponent, NavBarComponent, GlobalErrorHandlingComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
